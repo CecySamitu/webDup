6b05507b407acf788410a744862a78dc47ff0132
Cecy Samitu <cesfer99.cf@comunidad.unam.mx>
2025-01-23 15:49:45 -0600

    Vista general finalizada

--- javas.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const boton = document.querySelector('.boton');
-
-    boton.addEventListener('click', () => {
-        alert('¡Acción ejecutada!');
-    });
-});
